b844bd22a2cdfd0ad825ccd427967c0297abc3dc
ido-lempert <il.mrbit@gmail.com>
2023-12-01 13:31:33 +0200

    add logs

--- index.js
+++ index.js
@@ -13,9 +13,7 @@ const pool = new Pool({
     user: process.env.DB_USERNAME,
     host: process.env.DB_HOSTNAME,
     database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    ssl: true
+    password: process.env.DB_PASSWORD
 });
 
 const secret = 'T0p$ecreTT';
