dd800a8ddb1fb906872fb59209db175dc05079a8
ido-lempert <il.mrbit@gmail.com>
2023-12-01 13:49:23 +0200

    add logs

--- index.js
+++ index.js
@@ -117,8 +117,9 @@ app.post('/login', async (req, res) => {
     if (! (req.body.email && req.body.password)) return res.sendStatus(500);
 
     try {
-        const result = await pool.query('SELECT * FROM users WHERE email = $1 AND password = $2 RETURNING *', [req.body.email, req.body.password]);
-        console.log('result.rows[0]', result.rows[0]);
+        const result = await pool.query('SELECT * FROM users WHERE email = $1 AND password = $2', [req.body.email, req.body.password]);
+        if (!result.rows.length) return res.sendStatus(500);
+
         const {id, fullName, role} = result.rows[0];
 
         jwt.sign({id, fullName, role}, secret, { algorithm: 'HS256'}, (err, token)=>{
